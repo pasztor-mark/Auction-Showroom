192d5e6b0c34b72bd4b023fcaa7a2614386c47d2
Petrik Diak <diak@petrik.hu>
2024-04-26 09:32:21 +0200

    fix: faq szöveg

--- ShowroomProject/src/pages/Contact/Contact.tsx
+++ ShowroomProject/src/pages/Contact/Contact.tsx
@@ -30,16 +30,16 @@ export function ContactPage() {
                     </Col>
                         <Col sm={12} md={6}>
                             <Card className='text-center text-light bg-dark border border-primary rounded-3'>
-                                <CardHeader><h3>Looking to sell?</h3></CardHeader>
+                                <CardHeader><h3>Looking to buy?</h3></CardHeader>
                                 <CardBody>
-                                    <h5>How does the selling process work?</h5>
-                                    <p>The selling process is very simple. You can navigate to the Sell page, fill out the form to send us the details of your vehicle, and your car is up for auction on the Auction page after verification.</p>
-                                    <h5>How does the selling process get verified?</h5>
-                                    <p>The verification process is simple: you will be asked to provide papers for your vehicle via e-mail after your submission.</p>
-                                    <h5>How can I keep track of my listing?</h5>
-                                    <p>You can check the website often, and we will send you an e-mail whenever there is a new bid placed on your listing.</p>
-                                    <h5>Does this service come with any fees?</h5>
-                                    <p>No, the auction is 100% wallet-friendly on our side, however the prices of some listings might not be.</p>
+                                    <h5>How can I purchase a vehicle?</h5>
+                                    <p>If you outbid the other bidders, we will put you in contact with the seller, and from then onwards, the selling details are private between buyer and seller.</p>
+                                    <h5>Does the site take any cut in the purchase?</h5>
+                                    <p>Since the transaction is private, we have no access to any payments, therefore we will not be charging anything from the buyer, nor the seller.</p>
+                                    <h5>How can I keep track of my bids?</h5>
+                                    <p>We will send you an e-mail of your table of bids on every update. Make sure to check it regularly!</p>
+                                    <h5>Do you offer scam protection?</h5>
+                                    <p className='mb-3'>The transaction details are private, therefore you can only rely on your bank on helping you out with a scamming situation. All we can do is remove a listing or ban a user</p>
                                 </CardBody>
                             </Card>
                         </Col>
